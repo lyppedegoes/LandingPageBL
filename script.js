622ac6e33c0856c0f6cc338797bb18d4c1a9d0e6
Felipe de Goes <124632549+lyppedegoes@users.noreply.github.com>
2023-03-24 13:18:43 -0300

    break fix

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
- function copiarTexto() {
+function alterarBotao() {
+  document.getElementById('button-pix').onclick = function() {
+    this.textContent = 'Chave e-mail copiada!';
+  } 
+}
+
+
+function copiarTexto() {
         let textoCopiado = document.getElementById("texto");
         textoCopiado.select();
         textoCopiado.setSelectionRange(0, 99999);
         document.execCommand("copy");
-        alert("A chave e-mail foi copiada! " + textoCopiado.value);
-      }
+}
